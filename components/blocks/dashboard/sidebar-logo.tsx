0c4e2d184960755c739edf04f308f3a8b1e49ee0
Aminul Islam <5006546+AminulBD@users.noreply.github.com>
2026-01-03 05:23:23 +0600

    make link clickable

--- components/blocks/dashboard/sidebar-logo.tsx
+++ components/blocks/dashboard/sidebar-logo.tsx
@@ -3,23 +3,26 @@
 import * as React from 'react'
 import { Fan } from 'lucide-react'
 import { SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar'
+import Link from 'next/link'
 
 export function SidebarLogo() {
-
     return (
         <SidebarMenu>
             <SidebarMenuItem>
                 <SidebarMenuButton
                     size="lg"
                     className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+                    asChild
                 >
-                    <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center">
-                        <Fan className="size-4" />
-                    </div>
-                    <div className="grid flex-1 text-left text-sm leading-tight">
-                        <span className="truncate font-medium">Operation Media LLC</span>
-                        <span className="truncate text-xs">AdOps Pro</span>
-                    </div>
+                    <Link href="/dashboard">
+                        <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center">
+                            <Fan className="size-4" />
+                        </div>
+                        <div className="grid flex-1 text-left text-sm leading-tight">
+                            <span className="truncate font-medium">Operation Media LLC</span>
+                            <span className="truncate text-xs">AdOps Pro</span>
+                        </div>
+                    </Link>
                 </SidebarMenuButton>
             </SidebarMenuItem>
         </SidebarMenu>
